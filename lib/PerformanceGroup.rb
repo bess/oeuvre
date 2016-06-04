052bc06023e0814af037764d51d27201b5396628
Bess Sadler <bess@stanford.edu>
2016-06-04 15:20:09 -0700

    Added a dbpedia uri

--- lib/PerformanceGroup.rb
+++ lib/PerformanceGroup.rb
@@ -1,2 +1,3 @@
 class PerformanceGroup
+  attr_accessor :dbpedia_uri # a uri to a dbpedia endpoint for a performance group
 end
--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 require 'PerformanceGroup'
 
-describe 'a PerformanceGroup' do
+describe 'PerformanceGroup' do
   it 'can instantiate' do
     foo = PerformanceGroup.new
     expect(foo).to be_kind_of PerformanceGroup
+  end
+  it "has a dbpedia uri" do
+    foo = PerformanceGroup.new
+    foo.dbpedia_uri = "http://dbpedia.org/page/The_Beatles"
+    expect(foo.dbpedia_uri).to eql("http://dbpedia.org/page/The_Beatles")
   end
 end
